054c0601f5c34e913a6ff6ee99723e3e53d2e20f
Pedro López <plopez@financialforce.com>
2023-04-04 07:29:20 +0200

    Lesson 2: Modules

---
+++ NodeJS-Course/modules.js
@@ -0,0 +1,10 @@
+// Imports
+const { people, ages } = require('./people');
+
+console.log(people);
+console.log(ages);
+
+// Built in modules
+const os = require('os');
+
+console.log(os.platform(), os.homedir());
---
+++ NodeJS-Course/people.js
@@ -0,0 +1,7 @@
+const people = ['ryu', 'mario', 'yoshi', 'chun-li'];
+const ages = [10, 15, 20, 25];
+
+module.exports = {
+    people: people,
+    ages: ages
+};
