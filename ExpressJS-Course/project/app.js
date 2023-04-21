0a2cf3f225d2e9f598f5f2f0b4a5412d18ac0730
Pedro Lopez Biedma <plo@novatec-gmbh.de>
2023-04-21 09:53:04 +0200

    Lesson 7: View Engines

--- ExpressJS-Course/project/app.js
+++ ExpressJS-Course/project/app.js
@@ -3,6 +3,9 @@ const express = require('express');
 // Creating the Express App
 const app = express();
 
+// Register the view engine
+app.set('view engine', 'ejs');
+
 // Running the app
 app.listen(3000, () => {
     console.log('Listening for requests in http://localhost:3000/');
@@ -11,19 +14,21 @@ app.listen(3000, () => {
 // Listening for Requests
 app.get('/', (request, response) => {
     // response.send('<p>Home page</p>')
-    response.sendFile('/views/index.html', { root: __dirname })
+    // response.sendFile('/views/index.html', { root: __dirname })
+    response.render('index')
 })
 
 app.get('/about', (request, response) => {
-    response.sendFile('/views/about.html', { root: __dirname })
+    // response.sendFile('/views/about.html', { root: __dirname })
+    response.render('about')
 })
 
-// Redirects
-app.get('/about-me', (request, response) => {
-    response.redirect('/about');
+app.get('/blogs/create', (request, response) => {
+    response.render('create')
 })
 
 // 404 Page
 app.use((request, response) => {
-    response.status(404).sendFile('/views/404.html', { root: __dirname })
+    // response.status(404).sendFile('/views/404.html', { root: __dirname })
+    response.status(404).render('404')
 })
