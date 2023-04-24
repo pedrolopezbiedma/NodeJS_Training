a15a479d38b5847b98aca96a5ad69bbfd61dcf39
Pedro Lopez Biedma <plo@novatec-gmbh.de>
2023-04-24 16:57:37 +0200

    Lesson 9: Creating the blog model

---
+++ ExpressJS-Course/project/models/blog.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const blogSchema = new Schema({
+    title: { type: String, require: true },
+    snippet: { type: String, require: true },
+    body: { type: String, require: true },
+}, { 
+    timestamps: true
+});
+
+// Careful with the name inside the model method.
+// It will lowercase + pluralize it and search for that in the database collections, so in this case, 'blogs'
+const BlogModel = mongoose.model('Blog', blogSchema) 
+
+module.exports = Blog;
