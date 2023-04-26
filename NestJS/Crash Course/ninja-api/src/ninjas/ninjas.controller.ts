3d7e232bad71718774b9697816762d6c04d2832f
Pedro Lopez Biedma <plo@novatec-gmbh.de>
2023-04-26 11:04:02 +0200

    Lesson 3: Controllers

--- NestJS/Crash Course/ninja-api/src/ninjas/ninjas.controller.ts
+++ NestJS/Crash Course/ninja-api/src/ninjas/ninjas.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import {
+    Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { CreateNinjaDto } from './dto/create-ninja.dto';
 
 @Controller('ninjas')
-export class NinjasController {}
+export class NinjasController {
+  // GET /ninjas?[type=X] -- [ ninjas ]
+  @Get()
+  getNinjas(@Query('type') ninjaType: string) {
+    return [{ type: ninjaType }];
+  }
+
+  // GET /ninjas/:id -- { ninja }
+  @Get(':id')
+  getNinjaById(@Param('id') ninjaId: string) {
+    return { id: ninjaId };
+  }
+
+  // POST /ninjas -- { simple response }
+  @Post()
+  createNinja(@Body() newNinja: CreateNinjaDto) {
+    return { name: newNinja.name };
+  }
+
+  // PUT /ninjas/:id -- { updated ninja }
+  @Put('/:id')
+  updateNinjaById(@Param('id') ninjaId: string) {
+    return { id: ninjaId };
+  }
+
+  // DELETE /ninjas/:id -- { simple response }
+  @Delete('/:id')
+  deleteNinjaById(@Param('id') ninjaId: string) {
+    return { id: ninjaId };
+  }
+}
