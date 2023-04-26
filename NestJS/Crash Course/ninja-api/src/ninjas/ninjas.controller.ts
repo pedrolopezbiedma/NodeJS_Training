16a9028e96a8be0591facad868a1032c6e5bce28
Pedro Lopez Biedma <plo@novatec-gmbh.de>
2023-04-26 15:23:00 +0200

    Lesson 4: Services

--- NestJS/Crash Course/ninja-api/src/ninjas/ninjas.controller.ts
+++ NestJS/Crash Course/ninja-api/src/ninjas/ninjas.controller.ts
@@ -1,5 +1,5 @@
 import {
-    Body,
+  Body,
   Controller,
   Delete,
   Get,
@@ -9,36 +9,47 @@ import {
   Query,
 } from '@nestjs/common';
 import { CreateNinjaDto } from './dto/create-ninja.dto';
+import { NinjasService } from './ninjas.service';
+import { UpdateNinjaDto } from './dto/update-ninja.dto';
 
 @Controller('ninjas')
 export class NinjasController {
+  constructor(private ninjasService: NinjasService) {}
+
   // GET /ninjas?[type=X] -- [ ninjas ]
   @Get()
-  getNinjas(@Query('type') ninjaType: string) {
-    return [{ type: ninjaType }];
+  getNinjas(@Query('weapon') ninjaWeapon: string) {
+    return this.ninjasService.getNinjas(ninjaWeapon);
   }
 
   // GET /ninjas/:id -- { ninja }
   @Get(':id')
   getNinjaById(@Param('id') ninjaId: string) {
-    return { id: ninjaId };
+    return this.ninjasService.getNinjaById(ninjaId);
+    // return { id: ninjaId };
   }
 
   // POST /ninjas -- { simple response }
   @Post()
   createNinja(@Body() newNinja: CreateNinjaDto) {
-    return { name: newNinja.name };
+    return this.ninjasService.createNinja(newNinja);
+    // return { name: newNinja.name };
   }
 
   // PUT /ninjas/:id -- { updated ninja }
   @Put('/:id')
-  updateNinjaById(@Param('id') ninjaId: string) {
-    return { id: ninjaId };
+  updateNinjaById(
+    @Param('id') ninjaId: string,
+    @Body() updatedNinja: UpdateNinjaDto,
+  ) {
+    return this.ninjasService.updateNinja(ninjaId, updatedNinja);
+    // return { id: ninjaId };
   }
 
   // DELETE /ninjas/:id -- { simple response }
   @Delete('/:id')
   deleteNinjaById(@Param('id') ninjaId: string) {
-    return { id: ninjaId };
+    // return { id: ninjaId };
+    return this.ninjasService.removeNinja(ninjaId);
   }
 }
